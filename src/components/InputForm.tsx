4936c8f0a587728e4cf89abfcd2d5beeaf4520a9
siosw <git@siosw.net>
2024-03-11 12:23:43 +0100

    add token-list & dropdown placeholder

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
@@ -23,26 +30,39 @@ export function InputForm({ onSubmit, description }: { onSubmit: submitFunction;
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="amount"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Amount</FormLabel>
-              <FormControl>
-                <Input
-                  inputMode="numeric"
-                  {...field}
-                  value={field.value || ""}
-                  pattern="[0-9.]*"
-                  onChange={(e) => e.target.validity.valid && field.onChange(e.target.value)}
-                />
-              </FormControl>
-              <FormDescription>{description}</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="flex justify-center w-full">
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Amount</FormLabel>
+                <FormControl>
+                  <Input
+                    className="rounded-e-none border-e-0"
+                    inputMode="numeric"
+                    {...field}
+                    value={field.value || ""}
+                    pattern="[0-9.]*"
+                    onChange={(e) => e.target.validity.valid && field.onChange(e.target.value)}
+                  />
+                </FormControl>
+                <FormDescription>{description}</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Select>
+            <SelectTrigger className="rounded-s-none mt-8 w-[80px]">
+              <SelectValue defaultValue="ETH" placeholder="ETH" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="ETH">ETH</SelectItem>
+              <SelectItem value="USDC">USDC</SelectItem>
+              <SelectItem value="TEST">TEST</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
         <div className="flex justify-center">
           <Button className="w-32" type="submit">Bridge</Button>
         </div>
